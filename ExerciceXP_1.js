e5f81c8ad286938801b7ecb7d9a8359b29ba3c2b
VazDia <121106960+VazDia@users.noreply.github.com>
2023-01-12 01:52:58 +0000

    Update ExerciceXP_1.js

--- ExerciceXP_1.js
+++ ExerciceXP_1.js
@@ -20,7 +20,9 @@ function CalledWithParagraph(){
 
 setTimeout(CalledWithParagraph,2000);
 
-/* n your Javascript file, using setInterval, call a function every 2 seconds.
+/*Part III
+
+In your Javascript file, using setInterval, call a function every 2 seconds.
 The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
 The interval will be cleared (ie. clearInterval), when the user will click on the button.
 Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
