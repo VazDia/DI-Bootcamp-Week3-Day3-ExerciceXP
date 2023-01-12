11067aef3fc4a98644c1ba66d00a56d151e79f6e
VazDia <121106960+VazDia@users.noreply.github.com>
2023-01-12 01:56:58 +0000

    Update ExerciceXP_2.js

--- ExerciceXP_2.js
+++ ExerciceXP_2.js
@@ -1,14 +1,25 @@
+/**
+ Copy the code above, to a structured HTML file.
+In your Javascript file, use setInterval to move the <div id="animate"> to the right side of the <div id="container">, when the button is clicked on.
+The <div id="animate"> should move 1px to the right every milisecond, until it reaches the end of the <div id="container">.
+ */
 
-let divAnimate = document.getElementById("animate");
+ 
 
-function myMove() {
-  let pos = 0;
-  let moove = setInterval(function () {
-    if (pos == 350) {
-        clearInterval(moove);
-      }else{
-        pos += 1;
-        divAnimate.style.left = pos + "px";
-      }
-  },1);
+
+
+function myMove(){
+    let animate=document.getElementById("animate");
+    let container_width=400;
+    let animation_width=50;
+    let pos=0;
+    let move=setInterval(function(){
+        if (pos < (container_width-animation_width)){
+            pos++;
+            animate.style.left=pos+"px";
+        }else{
+            clearInterval(move);
+            return;
+        }
+    },1)
 }
