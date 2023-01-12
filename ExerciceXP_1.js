68fb09405f19974bb71313042df78719b25d38c9
VazDia <121106960+VazDia@users.noreply.github.com>
2023-01-12 01:50:42 +0000

    Update ExerciceXP_1.js

--- ExerciceXP_1.js
+++ ExerciceXP_1.js
@@ -1,34 +1,51 @@
-//Part I
-function hello() {
-  alert("Hello World!");
+/* Part I
+In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will alert “Hello World”. */
+
+// Creating the function
+function Called(){
+    alert("Hello World");
 }
 
-setTimeout(hello, 2000);
+setTimeout(Called,2000);
 
-//Part II
-
-let div = document.getElementById("container");
-
-let compte = 0;
-function pHello() {
-  let newP = document.createElement("p");
-  newP.innerText = "Hello World!";
-  div.appendChild(newP);
-  compte++;
-  if (compte == 5) {
-    clearInterval(pAdd);
-  }
+/* Part II
+In your Javascript file, using setTimeout, call a function after 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">. */
+function CalledWithParagraph(){
+    let p = document.createElement("p");
+    p.innerHTML="Hello World"
+    document.getElementById("container").append(p)
 }
 
-setTimeout(pHello, 2000);
+setTimeout(CalledWithParagraph,2000);
 
-//Part III
-let btn = document.getElementById("clear");
-
-btn.addEventListener("click", stop);
-
-function stop() {
-  clearInterval(pAdd);
+/* n your Javascript file, using setInterval, call a function every 2 seconds.
+The function will add a new paragraph <p>Hello World</p> to the <div id="container">.
+The interval will be cleared (ie. clearInterval), when the user will click on the button.
+Instead of clicking on the button, the interval will be cleared (ie. clearInterval) as soon as there will be 5 paragraphs inside the <div id="container">.
+*/
+function CalledByInterval(){
+    let p = document.createElement("p");
+    p.innerHTML="Hello World";
+    p.style.backgroundColor="green";
+    p.style.color="white";
+    if (document.getElementById("container").children.length < 5){
+        document.getElementById("container").append(p)
+    }
 }
 
-let pAdd = setInterval(pHello, 2000);
+let intervalle=setInterval(CalledByInterval,2000);
+
+let bouton=document.getElementById("clear");
+
+let div=document.getElementById("container");
+
+bouton.addEventListener("click",stopInterval);
+function stopInterval(){
+    clearInterval(intervalle);
+}
+
+
+
+
